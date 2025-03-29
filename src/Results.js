9fc6e247a9980240ad6e4136dea4337730d9c461
Katie <katherinesarff@gmail.com>
2025-03-29 08:50:30 -0500

    moved phonetics to its own element

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,8 @@ export default function Results(props){
     if (props.info){
     return(
         <div className="results">
-            <h2>{props.info.word} ({props.info.phonetic})</h2>
+            <h2>{props.info.word} </h2>
+            <h5><sm>{props.info.phonetic}</sm></h5>
             {props.info.meanings.map(function(meaning,index){ 
                 return (
                     <div key={index}>
